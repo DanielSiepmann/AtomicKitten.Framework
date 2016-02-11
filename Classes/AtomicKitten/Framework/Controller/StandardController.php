db4c24b7f1c2d6464faefa3fe7d4d956947b400d
Daniel Siepmann <coding@daniel-siepmann.de>
2016-02-11 21:04:45 +0100

    [TASK] Initial kickstart of package

---
+++ Classes/AtomicKitten/Framework/Controller/StandardController.php
@@ -0,0 +1,23 @@
+<?php
+namespace AtomicKitten\Framework\Controller;
+
+/*
+ * This file is part of the AtomicKitten.Framework package.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController
+{
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+
+}
